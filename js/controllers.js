b660b85f31ad6fce841e29f032240ba14e6cf0ce
Cliff Canan <cliff@nooch.com>
2015-07-02 03:39:49 -0400

    Initial commit!!

---
+++ js/controllers.js
@@ -0,0 +1,354 @@
+materialAdmin
+    // =========================================================================
+    // Base controller for common functions
+    // =========================================================================
+
+    .controller('materialadminCtrl', function($timeout, $state, growlService){
+        //Welcome Message
+        growlService.growl('Welcome back Mallinda!', 'inverse')
+        
+        
+        // Detact Mobile Browser
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+           angular.element('html').addClass('ismobile');
+        }
+
+        // By default Sidbars are hidden in boxed layout and in wide layout only the right sidebar is hidden.
+        this.sidebarToggle = {
+            left: false
+        }
+
+        // By default template has a boxed layout
+        this.layoutType = localStorage.getItem('ma-layout-status');
+        
+        // For Mainmenu Active Class
+        this.$state = $state;    
+        
+        //Close sidebar on click
+        this.sidebarStat = function(event) {
+            if (!angular.element(event.target).parent().hasClass('active')) {
+                this.sidebarToggle.left = false;
+            }
+        }
+    })
+
+
+    // =========================================================================
+    // Header
+    // =========================================================================
+    .controller('headerCtrl', function($timeout, messageService){
+
+        this.closeSearch = function(){
+            angular.element('#header').removeClass('search-toggled');
+        }
+        
+        // Get messages and notification for header
+        this.img = messageService.img;
+        this.user = messageService.user;
+        this.user = messageService.text;
+
+        this.messageResult = messageService.getMessage(this.img, this.user, this.text);
+
+
+        //Clear Notification
+        this.clearNotification = function($event) {
+            $event.preventDefault();
+            
+            var x = angular.element($event.target).closest('.listview');
+            var y = x.find('.lv-item');
+            var z = y.size();
+            
+            angular.element($event.target).parent().fadeOut();
+            
+            x.find('.list-group').prepend('<i class="grid-loading hide-it"></i>');
+            x.find('.grid-loading').fadeIn(1500);
+            var w = 0;
+            
+            y.each(function(){
+                var z = $(this);
+                $timeout(function(){
+                    z.addClass('animated fadeOutRightBig').delay(1000).queue(function(){
+                        z.remove();
+                    });
+                }, w+=150);
+            })
+            
+            $timeout(function(){
+                angular.element('#notifications').addClass('empty');
+            }, (z*150)+200);
+        }
+        
+        // Clear Local Storage - CLIFF: CAN BE DELETED, KEEPING JUST AS REFERENCE FOR CREATING A 'SWAL' DIALOG ALERT
+        this.clearLocalStorage = function() {
+            
+            //Get confirmation, if confirmed clear the localStorage
+            swal({   
+                title: "Are you sure?",   
+                text: "All your saved localStorage values will be removed",   
+                type: "success",   
+                showCancelButton: true,   
+                confirmButtonColor: "#F44336",   
+                confirmButtonText: "Yes, delete it!",   
+                closeOnConfirm: false 
+            }, function(){
+                localStorage.clear();
+                swal("Done!", "localStorage is cleared", "success"); 
+            });
+            
+        }
+        
+        //Fullscreen View
+        this.fullScreen = function() {
+            //Launch
+            function launchIntoFullscreen(element) {
+                if(element.requestFullscreen) {
+                    element.requestFullscreen();
+                } else if(element.mozRequestFullScreen) {
+                    element.mozRequestFullScreen();
+                } else if(element.webkitRequestFullscreen) {
+                    element.webkitRequestFullscreen();
+                } else if(element.msRequestFullscreen) {
+                    element.msRequestFullscreen();
+                }
+            }
+
+            //Exit
+            function exitFullscreen() {
+                if(document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if(document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if(document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen();
+                }
+            }
+
+            if (exitFullscreen()) {
+                launchIntoFullscreen(document.documentElement);
+            }
+            else {
+                launchIntoFullscreen(document.documentElement);
+            }
+        }
+    
+    })
+
+
+
+    // =========================================================================
+    // Best Selling Widget
+    // =========================================================================
+
+    .controller('bestsellingCtrl', function(bestsellingService){
+        // Get Best Selling widget Data
+        this.img = bestsellingService.img;
+        this.name = bestsellingService.name;
+        this.range = bestsellingService.range;
+        
+        this.bsResult = bestsellingService.getBestselling(this.img, this.name, this.range);
+    })
+
+ 
+    // =========================================================================
+    // Todo List Widget
+    // =========================================================================
+
+    .controller('todoCtrl', function(todoService){
+        
+        //Get Todo List Widget Data
+        this.todo = todoService.todo;
+        
+        this.tdResult = todoService.getTodo(this.todo);
+        
+        //Add new Item (closed by default)
+        this.addTodoStat = 0;
+        
+        //Dismiss
+        this.clearTodo = function(event) {            
+            this.addTodoStat = 0;
+            this.todo = '';
+        }
+    })
+
+
+    // =========================================================================
+    // Recent Items Widget
+    // =========================================================================
+
+    .controller('recentitemCtrl', function(recentitemService){
+        
+        //Get Recent Items Widget Data
+        this.id = recentitemService.id;
+        this.name = recentitemService.name;
+        this.parseInt = recentitemService.price;
+        
+        this.riResult = recentitemService.getRecentitem(this.id, this.name, this.price);
+    })
+
+
+    // =========================================================================
+    // Recent Posts Widget
+    // =========================================================================
+    
+    .controller('recentpostCtrl', function(recentpostService){
+        
+        //Get Recent Posts Widget Items
+        this.img = recentpostService.img;
+        this.user = recentpostService.user;
+        this.text = recentpostService.text;
+        
+        this.rpResult = recentpostService.getRecentpost(this.img, this.user, this.text);
+    })
+
+
+    //=================================================
+    // Profile
+    //=================================================
+
+    .controller('profileCtrl', function(growlService){
+        
+        //Get Profile Information from profileService Service
+        
+        //User
+        this.accountStatus = "Identity Verified";
+        this.fullName = "Josh Hamilton";
+        this.birthDay = "23/06/1982";
+        this.mobileNumber = "(215) 711-6789";
+        this.emailAddress = "josh.h@nooch.com";
+        this.twitter = "@NoochMoney";
+        this.addressSuite = "1098 ABC Towers";
+        this.addressCity = "Philadelphia, PA";
+        this.addressCountry = "United States";
+        this.ssnLast4 = "7654";
+    
+    
+        //Edit
+        this.editSummary = 0;
+        this.editInfo = 0;
+        this.editContact = 0;
+    
+        
+        this.submit = function(item, message) {            
+            if(item === 'profileSummary') {
+                this.editSummary = 0;
+            }
+            
+            if(item === 'profileInfo') {
+                this.editInfo = 0;
+            }
+            
+            if(item === 'profileContact') {
+                this.editContact = 0;
+            }
+            
+            growlService.growl(message+' has updated Successfully!', 'inverse'); 
+        }
+
+    })
+
+
+
+    //=================================================
+    // LOGIN
+    //=================================================
+
+    .controller('loginCtrl', function(){
+        
+        //Status
+    
+        this.login = 1;
+        this.register = 0;
+        this.forgot = 0;
+    })
+
+
+    //=================================================
+    // CALENDAR
+    //=================================================
+    
+    .controller('calendarCtrl', function(){
+    
+        //Create and add Action button with dropdown in Calendar header. 
+        this.month = 'month';
+    
+        this.actionMenu = '<ul class="actions actions-alt" id="fc-actions">' +
+                            '<li class="dropdown">' +
+                                '<a href="" data-toggle="dropdown"><i class="md md-more-vert"></i></a>' +
+                                '<ul class="dropdown-menu dropdown-menu-right">' +
+                                    '<li class="active">' +
+                                        '<a data-calendar-view="month" href="">Month View</a>' +
+                                    '</li>' +
+                                    '<li>' +
+                                        '<a data-calendar-view="basicWeek" href="">Week View</a>' +
+                                    '</li>' +
+                                    '<li>' +
+                                        '<a data-calendar-view="agendaWeek" href="">Agenda Week View</a>' +
+                                    '</li>' +
+                                    '<li>' +
+                                        '<a data-calendar-view="basicDay" href="">Day View</a>' +
+                                    '</li>' +
+                                    '<li>' +
+                                        '<a data-calendar-view="agendaDay" href="">Agenda Day View</a>' +
+                                    '</li>' +
+                                '</ul>' +
+                            '</div>' +
+                        '</li>';
+        
+        
+    
+        //Calendar Event Data
+        this.calendarData = {
+            eventName: ''
+        };
+    
+        //Tags
+        this.tags = [
+            'bgm-teal',
+            'bgm-red',
+            'bgm-pink',
+            'bgm-blue',
+            'bgm-lime',
+            'bgm-green',
+            'bgm-cyan',
+            'bgm-orange',
+            'bgm-purple',
+            'bgm-gray',
+            'bgm-black',
+        ]
+        
+        this.onTagClick = function(tag, $index) {
+            this.activeState = $index;
+            this.activeTagColor = tag;
+        } 
+            
+        //Open new event modal on selecting a day
+        this.onSelect = function(argStart, argEnd) {
+            $('#addNew-event').modal('show');   
+            this.calendarData.getStart = argStart;
+            this.calendarData.getEnd = argEnd;
+        }
+        
+        //Add new event
+        this.addEvent = function() {
+            var tagColor = $('.event-tag > span.selected').data('tag');
+
+            if (this.calendarData.eventName.length > 0) {
+
+                //Render Event
+                $('#calendar').fullCalendar('renderEvent',{
+                    title: this.calendarData.eventName,
+                    start: this.calendarData.getStart,
+                    end:  this.calendarData.getEnd,
+                    allDay: true,
+                    className: this.activeTagColor
+
+                },true ); //Stick the event
+
+                this.activeState = -1;
+                this.calendarData.eventName = '';
+                $('#addNew-event').modal('hide');
+            }
+        }
+
+        
+    })
