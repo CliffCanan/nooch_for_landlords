608468849992be8e11db8f53ab75bf7740314b14
malkitsingh <malkitsingh01@gmail.com>
2015-08-10 16:26:02 +0530

    dynamic data is being displayed now at profile-about and home page

--- js/appSettings.js
+++ js/appSettings.js
@@ -1,8 +1,8 @@
 ﻿
 //for local machine
-var PATH = 'http://localhost:62942/api/';
+//var PATH = 'http://localhost:62942/api/';
 //for dev server
-//var PATH = 'http://54.201.43.89/api/';
+var PATH = 'http://54.201.43.89/api/';
 
 var URLs = {
     Login: PATH + 'Users/Login',
