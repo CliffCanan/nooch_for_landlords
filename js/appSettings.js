b86914051e56b62267b473ab567ac5f55d0703bd
malkitsingh <malkitsingh01@gmail.com>
2015-08-10 16:43:44 +0530

    corrected base url

--- js/appSettings.js
+++ js/appSettings.js
@@ -2,7 +2,7 @@
 //for local machine
 //var PATH = 'http://localhost:62942/api/';
 //for dev server
-var PATH = 'http://54.201.43.89/api/';
+var PATH = 'http://54.201.43.89/Landlord_APIs/api/';
 
 var URLs = {
     Login: PATH + 'Users/Login',
