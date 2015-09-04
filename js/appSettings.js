78ef2aec1d37f2b02287b7936e9365fc8b763c2c
malkitsingh <malkitsingh01@gmail.com>
2015-09-04 15:25:17 +0530

    fixed base url error

--- js/appSettings.js
+++ js/appSettings.js
@@ -4,7 +4,7 @@
 //var PATH = 'http://localhost:42865/db/api/';
 
 //for dev server
-var PATH = 'http://54.201.43.89/Landlord_APIs/api/';
+var PATH = 'http://54.201.43.89/landlords/db/api/';
 
 var URLs = {
     Login: PATH + 'Users/Login',
